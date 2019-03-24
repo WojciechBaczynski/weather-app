4262f67e6e04ed9c4fb337675e50256e49c3228c
Wojciech Baczyński <wp.baczynski@gmail.com>
2019-03-24 17:55:37 +0100

    Add connection between API and state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Form from './Form/Form';
 import Result from './Result/Result';
 import './App.css';
 
+const APIkey = '9cb506a12379e91097ea37e115e442a2';
+
 class App extends Component {
   state = {
     value: "",
@@ -24,7 +26,7 @@ class App extends Component {
 
   submitCityHandler = (e) => {
     e.preventDefault();
-    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=9cb506a12379e91097ea37e115e442a2`;
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIkey}&units=metric`;
 
     fetch(API)
       .then(res => {
@@ -34,8 +36,26 @@ class App extends Component {
         throw Error("Could not get a response");
       })
       .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
+      .then(data => {
+        const time = new Date().toLocaleString();
+        this.setState(prevState => ({
+          date: time,
+          city: prevState.value,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          wind: data.wind.speed,
+          err: false
+        }));
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState(prevState => ({
+          err: true,
+          city: prevState.value
+        }));
+      });
   }
 
   render() {
@@ -45,7 +65,7 @@ class App extends Component {
           value={this.state.value}
           submitCity={this.submitCityHandler}
           inputChange={this.inputChangeHandler} />
-        <Result />
+        <Result weather={this.state} />
       </div>
     );
   }
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const result = props => (
-    <p>Result</p>
-);
+const result = props => {
+    const { err, city, temp } = props.weather;
+    return (
+        <React.Fragment>
+            <p>{String(err)}</p>
+            <p>Miasto: {city}</p>
+            <p>Temperatura: {temp}</p>
+        </React.Fragment>
+    );
+};
 
 export default result;
