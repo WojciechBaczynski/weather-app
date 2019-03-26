c00e51c91f3629c3eccb0413b66da33e3d7fc7d4
Wojciech Baczyński <wp.baczynski@gmail.com>
2019-03-26 14:29:13 +0100

    Updates

---
+++ precache-manifest.2249689e3bfff368ab178041ec150f65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4742023bc07e1b8dccd6",
+    "url": "/weather-app/static/css/main.1024eb89.chunk.css"
+  },
+  {
+    "revision": "4742023bc07e1b8dccd6",
+    "url": "/weather-app/static/js/main.ad2aa3d8.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "694ce763c2230c53bc01",
+    "url": "/weather-app/static/js/2.cf02559e.chunk.js"
+  },
+  {
+    "revision": "fefc386a4b5c9b31cd8eb0572dde2905",
+    "url": "/weather-app/index.html"
+  }
+];
